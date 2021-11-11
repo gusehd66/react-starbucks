06155ce844b19469d22516b4dd53b36b240ed7fb
gusehd66 <rlagusehd45@naver.com>
2021-11-11 14:13:12 +0900

    í¾¨ Home 컴포넌트 분리
    Header, Footer 를 제외한 컴포넌트 합성
    
    SCSS 문법 수정

--- src/App.js
+++ src/App.js
@@ -1,32 +1,14 @@
 import { HashRouter } from "react-router-dom";
 import "./App.scss";
-import Awards from "./component/Awards";
-import Favorite from "./component/Favorite";
-import FindStore from "./component/FindStore";
 import Footer from "./component/Footer";
 import Header from "./component/Header";
-import Notice from "./component/Notice";
-import Product from "./component/Product";
-import ReserveCoffee from "./component/ReserveCoffee";
-import ReserveStore from "./component/ReserveStore";
-import Rewards from "./component/Rewards";
-import Visual from "./component/Visual";
-import Youtube from "./component/Youtube";
+import Home from "./route/Home";
 
 function App() {
   return (
     <HashRouter>
       <Header />
-      <Visual />
-      <Notice />
-      <Rewards />
-      <Youtube />
-      <Product />
-      <ReserveCoffee />
-      <Favorite />
-      <ReserveStore />
-      <FindStore />
-      <Awards />
+      <Home />
       <Footer />
     </HashRouter>
   );
--- src/route/Home.jsx
+++ src/route/Home.jsx
@@ -1,3 +1,29 @@
-const Home = () => {};
+import Awards from "../component/Awards";
+import Favorite from "../component/Favorite";
+import FindStore from "../component/FindStore";
+import Notice from "../component/Notice";
+import Product from "../component/Product";
+import ReserveCoffee from "../component/ReserveCoffee";
+import ReserveStore from "../component/ReserveStore";
+import Rewards from "../component/Rewards";
+import Visual from "../component/Visual";
+import Youtube from "../component/Youtube";
+
+const Home = () => {
+  return (
+    <>
+      <Visual />
+      <Notice />
+      <Rewards />
+      <Youtube />
+      <Product />
+      <ReserveCoffee />
+      <Favorite />
+      <ReserveStore />
+      <FindStore />
+      <Awards />
+    </>
+  );
+};
 
 export default Home;
